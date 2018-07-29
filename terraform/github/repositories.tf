89ee86f326b36812a437d15372d4c8caec1b25a2
Rémy Greinhofer <remy.greinhofer@gmail.com>
2018-07-29 16:23:20 -0500

    Add github plan

--- terraform/github/repositories.tf
+++ terraform/github/repositories.tf
@@ -1,4 +1,8 @@
 resource "github_repository" "landing-page" {
-  name        = "landing-page"
-  description = "RYR landing page"
+  name                = "landing-page"
+  description         = "RYR landing page"
+  allow_merge_commit  = false
+  has_downloads       = true
+  has_issues          = true
+  has_wiki            = true
 }
